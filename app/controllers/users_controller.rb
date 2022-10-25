4ac095f53b7b4b89749b60886e98db0643eed0e0
Bekzatgit config --global user.name <serikbayevb05@gmail.com>
2022-10-25 22:51:47 +0500

    add users controller and create action to register a user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.create(user_params)
+
+        if @user.valid?
+            toke = encode_token({ user_id: @user.id })
+            render json: { user: @user, token: token }, status: :ok
+        else
+            render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+end
