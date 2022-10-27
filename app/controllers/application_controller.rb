7722e79872a0e196dd3f66e42854d0c1fd4be771
Bekzatgit config --global user.name <serikbayevb05@gmail.com>
2022-10-27 17:03:49 +0500

    add decode_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, 'secret')
     end
+    def decode_token
+        # eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMTIzIn0.AOXUao_6a_LbIcwkaZU574fPqvW6mPvHhwKC7Fatuws
+        auth_header = request.headers['Authorization']
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
