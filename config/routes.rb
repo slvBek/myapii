f4797ac9047e3a85c2aa9732b94906d7edde8f25
Bekzatgit config --global user.name <serikbayevb05@gmail.com>
2022-10-27 18:19:11 +0500

    add video

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :videos
   resource :users, only: [:create]
   post "/login", to: "users#login"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_160502) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_124326) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -18,4 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_160502) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
+  end
+
+  add_foreign_key "videos", "users"
 end
