f5c927aac51dbf752bcde3e94b6a9f4901507e9c
Bekzatgit config --global user.name <serikbayevb05@gmail.com>
2022-10-31 01:04:22 +0500

    api

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   resources :movies
   resources :videos
   resource :users, only: [:signup, :create]
-  post "/signup", to: "users#signup"
   post "/login", to: "users#login"
+  post "/signup", to: "users#signup"
   post "/logout", to: "users#logout"
-
 
 end
