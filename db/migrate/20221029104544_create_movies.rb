6f0f7df02ff24d1d3a249fa1abd02740aeeb8ac8
Bekzatgit config --global user.name <serikbayevb05@gmail.com>
2022-10-31 15:55:19 +0500

    fixed some bugs

--- db/migrate/20221029104544_create_movies.rb
+++ db/migrate/20221029104544_create_movies.rb
@@ -3,7 +3,6 @@ class CreateMovies < ActiveRecord::Migration[7.0]
     create_table :movies do |t|
       t.string :title
       t.string :quote
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
